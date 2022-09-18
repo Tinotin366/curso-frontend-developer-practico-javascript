2ce5fdf59c5565c4c62c76744bbde7e455bbd157
Tino Echaniz <tino@tinux.net>
2022-09-18 23:25:50 +0200

    Actualizado para el menu-mobile

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+// JS Para la hamburguesa 
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-  console.log('Click');
+//   console.log('Click');
   desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    // console.log('Click');
+    mobileMenu.classList.toggle('inactive');
+  }
+  
