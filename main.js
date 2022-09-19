8a1875c487e4b3d20855e72ab8f6ad75be97681c
Tino Echaniz <tino@tinux.net>
2022-09-19 11:33:26 +0200

    Mostrar Ocultar Menus y shoppin Cart Desktop y Mobile

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+// JS Para el carrito de compras
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
 //   console.log('Click');
@@ -18,4 +24,9 @@ function toggleMobileMenu() {
     // console.log('Click');
     mobileMenu.classList.toggle('inactive');
   }
+
+function toggleCarritoAside() {
+  // console.log('Click');
+  aside.classList.toggle('inactive');
+}
   
